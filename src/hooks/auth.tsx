ee3986fcb8ca7e9fa5c068a10505d7cce84a885b
Samuel Braga <samuel.fantini.braga@hotmail.com>
2020-10-01 17:11:51 -0300

    Criando estrutura do dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -10,7 +10,7 @@ interface User {
   id: string;
   name: string;
   email: string;
-  avatar: string;
+  avatar_url: string;
   created_at: Date;
   updated_at: Date;
 }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,71 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppoitment,
+  Calendar,
+} from './styles';
+import { useAuth } from '../../hooks/auth';
+
+import logoImg from '../../assets/logo.svg';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppoitment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/18483769?s=460&u=79d87b8315cae91b36d7390fb18f8637488bf434&v=4"
+                alt="Samuel Braga"
+              />
+
+              <strong>Samuel Braga</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppoitment>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
